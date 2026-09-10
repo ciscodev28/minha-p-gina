75bdc16939f05517c43085f0e18f814455a304c3
ciscodev28 <augusto.fco28@gmail.com>
2026-09-10 14:19:15 +0000

    alterações.2

--- meu_app_flask/app.py
+++ meu_app_flask/app.py
@@ -10,9 +10,9 @@ def home():
 def sobre():
     return render_template('sobre.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-@app.route("/favicon")
-def favicon():
-    return app.send_static_file("favicon.cio")
